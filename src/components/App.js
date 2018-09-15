461fd354ef0b62e428cc608a71c6cd923f9226bf
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-15 09:35:35 -0500

    aj styles

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 class App extends Component {
   render(){
     return (
-    <div>
+    <div className='App'>
       <div>Countdown to Extinction (11/18/2018)</div>
       <div>14 Days</div>
       <div>30 Hours</div>
       <div>15 Minutes</div>
       <div>20 Seconds</div>
+      <br />
       <div>
         <input placeholder='New Date' />
         <button>Submit</button>
