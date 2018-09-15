846795e22443089c6bc184794fa8dbb71a6a29af
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-15 09:30:22 -0500

    skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class App extends Component {
   render(){
     return (
-    <div>Countdown to Extinction</div>
+    <div>
+      <div>Countdown to Extinction (11/18/2018)</div>
+      <div>14 Days</div>
+      <div>30 Hours</div>
+      <div>15 Minutes</div>
+      <div>20 Seconds</div>
+    </div>
     )
   }
 }
