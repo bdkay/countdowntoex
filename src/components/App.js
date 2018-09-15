b03074416c344f9100bf3d112168e35d5ad3481c
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-15 09:36:45 -0500

    aj styles 2

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ class App extends Component {
   render(){
     return (
     <div className='App'>
-      <div>Countdown to Extinction (11/18/2018)</div>
-      <div>14 Days</div>
-      <div>30 Hours</div>
-      <div>15 Minutes</div>
-      <div>20 Seconds</div>
+      <div className='App-title'>Countdown to Extinction (11/18/2018)</div>
+      <div className='Clock-days'>14 Days</div>
+      <div className='Clock-hours'>30 Hours</div>
+      <div className='Clock-minutes'>15 Minutes</div>
+      <div className='Clock-seconds'>20 Seconds</div>
       <br />
       <div>
         <input placeholder='New Date' />
